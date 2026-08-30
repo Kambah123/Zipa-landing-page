0485d828390f158143fcef3c9bcc3628b62cf5ac
MUSA SULAIMAN <sulaimanmusa399@gmail.com>
2026-08-30 12:39:33 +0100

    feat: add ambassador routes

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -2,13 +2,13 @@ import { APP_URL } from "./app";
 
 export const Routes = {
     Home: "/",
-
     Difference: "/#difference",
     Workflow: "/#workflow",
     Capabilities: "/#capabilities",
     Integrations: "/#integrations",
     Membership: "/#membership",
     Faq: "/#faq",
-
     Dashboard: `${APP_URL}/login`,
+    Ambassadors: "/ambassadors",
+    AmbassadorsThankYou: "/ambassadors/thank-you",
 } as const;
